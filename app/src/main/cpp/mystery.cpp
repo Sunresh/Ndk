4caa374021ea2a57201008868076dffb96409f63
sunresh <hinareshg@gmail.com>
2024-07-15 14:46:06 +0900

    k

--- app/src/main/cpp/mystery.cpp
+++ app/src/main/cpp/mystery.cpp
@@ -2,21 +2,46 @@
 #include <jni.h>
 #include "camera.cpp"
 
-extern "C"
-{
-	JNIEXPORT jstring JNICALL Java_com_mycompany_myapp_MainActivity_stringFromJNI(JNIEnv* env, jobject thiz)
-	{
-		return env->NewStringUTF("Hello from JNI !");
-	}
-	JNIEXPORT jdouble JNICALL Java_com_mycompany_myapp_MainActivity_cCircle(JNIEnv* env, jobject thiz, jdouble radius)
-    {
-        const double pi = 3.14159;
-        return pi * radius * radius;
+extern "C" {
+
+// Shorter function names
+jstring stringFromJNI(JNIEnv* env, jobject thiz) {
+    return env->NewStringUTF("Hello from JNI !");
+}
+
+jdouble cCircle(JNIEnv* env, jobject thiz, jdouble radius) {
+    const double pi = 3.14159;
+    return pi * radius * radius;
+}
+
+jdouble cRectangle(JNIEnv* env, jobject thiz, jdouble length, jdouble width) {
+    return length * width;
+}
+
+// Array of native methods to be registered
+static JNINativeMethod methods[] = {
+    {"stringFromJNI", "()Ljava/lang/String;", (void*)stringFromJNI},
+    {"cCircle", "(D)D", (void*)cCircle},
+    {"cRectangle", "(DD)D", (void*)cRectangle}
+};
+
+// JNI_OnLoad function to register the native methods
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env;
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return -1;
     }
 
-    // Function to calculate the area of a rectangle
-    JNIEXPORT jdouble JNICALL Java_com_mycompany_myapp_MainActivity_cRectangle(JNIEnv* env, jobject thiz, jdouble length, jdouble width)
-    {
-        return length * width;
+    jclass clazz = env->FindClass("com/mycompany/myapp/MainActivity");
+    if (clazz == nullptr) {
+        return -1;
     }
+
+    if (env->RegisterNatives(clazz, methods, sizeof(methods) / sizeof(methods[0])) < 0) {
+        return -1;
+    }
+
+    return JNI_VERSION_1_6;
 }
+
+} // extern "C"
